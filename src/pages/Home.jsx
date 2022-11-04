21d708316bb1f1849dd2a12541fa2180c4f15297
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-04 04:53:35 +0100

    refactor: restructuring code

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Profile from '../components/Profile'
+import LinkButton from '../components/LinkButton';
+import SocialIcons from '../components/SocialIcons';
+
+const Home = () => {
+  return (
+    <>
+      <Profile />
+      <LinkButton />
+      <SocialIcons />
+    </>
+  )
+}
+
+export default Home
