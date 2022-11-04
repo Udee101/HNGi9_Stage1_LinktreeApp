e36cc1a5bd410423511435edb6edc20b87d2b314
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-04 03:31:15 +0100

    refactor: create array of objects for the links

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,9 +1,53 @@
 import React from "react";
 
-const LinkButton = (props) => {
+const links = [
+  {
+    linkName : 'Twitter Link',
+    linkId: 'my_twitter',
+    linkUrl: 'https://twitter.com/theZuriTeam',
+  },
+  {
+    linkName : 'Zuri Team',
+    linkId: 'btn__zuri',
+    linkUrl: 'https://training.zuri.team/',
+  },
+  {
+    linkName : 'Zuri Books',
+    linkId: 'books',
+    linkUrl: 'https://books.zuri.team',
+  },
+  {
+    linkName : 'Python Books',
+    linkId: 'btn__python',
+    linkUrl: 'https://books.zuri.team/python-for-beginners?ref_id=Frankenstylz',
+  },
+  {
+    linkName : 'Background Check for Coders',
+    linkId: 'pitch',
+    linkUrl: 'https://backgroundcheck.zuri.team',
+  },
+  {
+    linkName : 'Design Books',
+    linkId: 'book__design',
+    linkUrl: 'https://books.zuri.team/design-rules',
+  },
+  {
+    linkName : 'Contact Me',
+    linkId: 'contact',
+    linkUrl: '/contact',
+  }
+]
+
+const LinkButton = () => {
   return (
-    <div className="links">
-      <h2>{props.name}</h2>
+    <div className="link_section">
+      {
+        links.map((link, i) => {
+          return (
+            <a href={link.linkUrl} id={link.linkId} key={i} rel="noreferrer" >{link.linkName}</a>
+          )
+        })
+      }
     </div>
   )
 }
