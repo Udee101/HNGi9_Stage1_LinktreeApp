33b3f17fe81819ef6a9b434beb6e45e9587dd53c
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-05 03:44:13 +0100

    feat: adding contact me page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from '../components/Form/Form'
+
+const Contact = () => {
+  return (
+    <div className='contact_section'>
+      <header>
+        <p className='heading'>Contact Me</p>
+        <p className='desc'>Hi there, contact me to ask about anything  you have in mind.</p>
+      </header>
+      <Form />
+    </div>
+  )
+}
+
+export default Contact
