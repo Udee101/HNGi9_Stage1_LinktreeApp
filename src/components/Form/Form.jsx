0623377f011deb753294843370fccf32017a6966
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-05 04:06:59 +0100

    freat: create form layout

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -32,7 +32,32 @@ const inputs = [
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <>
+      <form>
+        {inputs.map((input, i) => { 
+          return(
+            <div className={input.className} key={i}>
+              <FormInput 
+                 {...input} 
+                id={input.id}
+                hint={input.hint}
+              />
+            </div>
+          )          
+        })}
+
+        <div className='message_box'>
+          <p className='label'>Message</p>
+          <textarea name="message" id="message" cols="30" rows="10" placeholder="Send me a message asnd I'll reply as soon as possible" ></textarea>
+        </div>
+
+        <div className="check">
+          <input type="checkbox" name="checkbox" id="check_box" required/><span>You agree to providing your data to Frank Udeorah who may contact you.</span>
+        </div>
+
+        <button id='btn__submit' type='submit'>Send message</button>
+      </form>
+    </>
   )
 }
 
